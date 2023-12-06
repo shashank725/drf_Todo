08a2b7a9649b5e5794fe2e3676fd5b23fb39353a
shashank725 <shashank.nov725@gmail.com>
2023-12-07 03:25:11 +0530

    [+] models & admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import TodoItem
 
 # Register your models here.
+
+#Admin changelist 
+@admin.action(description="Mark selected items' status as Open")
+def make_Open(modeladmin, request, queryset):
+    queryset.update(status='OPEN')
+@admin.action(description="Mark selected items' status as Working")
+def make_Working(modeladmin, request, queryset):
+    queryset.update(status='WORKING')
+@admin.action(description="Mark selected items' status as Done")
+def make_Done(modeladmin, request, queryset):
+    queryset.update(status='DONE')
+@admin.action(description="Mark selected items' status as Overdue")
+def make_Overdue(modeladmin, request, queryset):
+    queryset.update(status='OVERDUE')
+
+# @admin.register(TodoItem)     #using decorator
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ["title", "due_date", "status"]
+    # ordering = ["title"]
+    date_hierarchy = "timestamp"
+    actions = [make_Open, make_Working, make_Done, make_Overdue]
+    empty_value_display = "-empty-"
+    # list_filter = ["title", "due_date", "tags", "status"]
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields":["title", "description"],
+            },
+        ),
+        (
+            None,
+            {
+                "fields": ["tags", "status"]
+            }
+        ),
+        (
+            "Dates :",
+            {
+                "classes": ["collapse"],
+                "fields": ["timestamp", "due_date"],
+            }
+        ),
+    ]
+
+admin.site.register(TodoItem, TodoItemAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoItem(models.Model):
+    STATUS_CHOICES = [
+        ('OPEN', 'Open'),
+        ('WORKING', 'Working'),
+        ('DONE', 'Done'),
+        ('OVERDUE', 'Overdue'),
+    ]
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, required=True)
+    description = models.TextField(max_length=1000, required=True)
+    due_date = models.DateField(null=True, blank=True)
+    # tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.CharField(null=True, blank=True, max_length=500)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, 
+                              default='OPEN')
+    
+    def save(self, *args, **kwargs):
+        #To save unique tags
+        if self.tags:
+            tag = self.tags.replace(" ", "")
+            tag = tag.split(",")
+            tag = set(tag)
+            self.tags = ", ".join(tag)
+        super().save(*args, **kwargs)  #Call the "real" save() method.
+
+
+# class Tag(models.Model):
+#     value = models.CharField(max_length=50, unique=True)
+
+#     def __str__(self):
+#         return self.value
